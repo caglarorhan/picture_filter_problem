8360262916e269f8d7b95ab5897df32f18c46870
caglarorhan <caglaror@gmail.com>
2022-10-14 03:52:19 -0500

    Blur effect filter

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-### Picture filter for the given picture data, based on CS50 lecture notes
+### Picture filter (blur) for the given picture data, based on CS50 lecture notes
 
 > https://jsfiddle.net/caglaror/4pod21Lv/
--- filter.js
+++ filter.js
@@ -1,7 +1,9 @@
 function createPixel(){
     let pixel =[];
-    let val = Math.random()*10;
-    pixel.push(Math.floor(val*1000));
+    let pRed = Math.floor(Math.random()*256);
+    let pGreen = Math.floor(Math.random()*256);
+    let pBlue = Math.floor(Math.random()*256);
+    pixel.push({pRed: pRed, pGreen: pGreen, pBlue: pBlue});
     return pixel;
 }
 
@@ -19,7 +21,7 @@ function createPicture(y,x){
 
 
 
-function applyFilter(pictureData, toNewArray){
+function applyFilter(pictureData){
     if(typeof pictureData !== 'object' || !Array.isArray(pictureData)){
         console.log(`Given data is not an object and array.`);
         return false;
@@ -28,31 +30,58 @@ function applyFilter(pictureData, toNewArray){
 
     for(let i=0; i<pictureData.length; i++){
         for(let j=0; j<pictureData[i].length; j++){
-            let cumulativeValue = 0;
+            let cumulativeValues = {pRed:0,pGreen:0,pBlue:0};
             let countedPixels = 0;
             for(let k=-1; k<=1;k++){
                 for(let l=-1; l<=1; l++){
                     if(((i+k)>=0 && (i+k)<pictureData.length) && ((j+l)>=0 && (j+l)<pictureData[i].length)){
-                        //console.log(i+k, j+l);
-                        cumulativeValue += pictureData[i+k][j+l][0];
+                        // console.log(pictureData[i+k][j+l]);
+                        // console.log(cumulativeValues)
+                        cumulativeValues.pRed += pictureData[i+k][j+l][0].pRed;
+                        cumulativeValues.pGreen += pictureData[i+k][j+l][0].pGreen;
+                        cumulativeValues.pBlue += pictureData[i+k][j+l][0].pBlue;
+                        //console.log(cumulativeValues)
                         countedPixels++;
                     }
                 }
             }
             //console.log(countedPixels);
-            let avgValue = cumulativeValue / countedPixels;
-            //console.log(avgValue);
-            if(toNewArray){
-                filteredPicture[i][j].push(Math.floor(avgValue));
-            }else{
-                pictureData[i][j].push(Math.floor(avgValue));
+            let avgValue = {
+                pRed: Math.floor(cumulativeValues.pRed/countedPixels),
+                pGreen: Math.floor(cumulativeValues.pGreen/countedPixels),
+                pBlue: Math.floor(cumulativeValues.pBlue/countedPixels)
             }
+            //console.log(avgValue);
+                filteredPicture[i][j].push(avgValue);
+
         }
     }
-    return toNewArray ? filteredPicture: pictureData;
+    //console.log(filteredPicture)
+    return filteredPicture;
+}
+
+function drawPicture(pictureData=[]) {
+    let pixelTemp = (colorCode)=>{
+        //console.log(`${colorCode.pRed,colorCode.pGreen,colorCode.pBlue}`)
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = " ";
+        newDiv.style.cssText = `display:inline-block; background-color: rgb(${colorCode.pRed}, ${colorCode.pGreen}, ${colorCode.pBlue}); width: 1px; height: 1px;`;
+        return newDiv;
+    }
+    for(let i=0; i<pictureData.length; i++) {
+        for(let j=0; j<pictureData[i].length; j++){
+            document.getElementById('container').appendChild(pixelTemp(pictureData[i][j][0]));
+        }
+    }
+
 }
 
 
-let pD = createPicture(8,8);
-console.log(applyFilter(pD,false))
-//applyFilter(pD);
+
+window.addEventListener("load", () => {
+    let pPD = createPicture(500,500);
+    let fPD = applyFilter(pPD);
+    console.log(fPD);
+drawPicture(pPD);
+    drawPicture(fPD);
+})
